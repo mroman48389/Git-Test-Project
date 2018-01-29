af8244c3feb2f1611cd5a5af6fcb63f1393a5cd7
mroman48389 <mroman@avenirhealth.org>
2018-01-29 15:54:11 -0500

    Initial commit

---
+++ Unit1.pas
@@ -0,0 +1,46 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  button1.caption := 'I was clicked, yes!';
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  button2.caption := 'I was clicked, yay!'; // change
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+  button1.caption := 'Please click me!';
+  button2.caption := 'Click me too!';
+end;
+
+end.
